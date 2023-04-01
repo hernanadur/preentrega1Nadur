af482fce97cfbc2b02cf859d03faf8db8ec8fc69
Hernan Nadur <hernanadur@gmail.com>
2023-03-31 23:13:47 -0300

    incorporando js a mi proyecto

---
+++ README.md
@@ -0,0 +1,22 @@
+
+# Project Title
+
+Primer Pre entrega de Proyecto final GESM
+## Authors
+
+- Nadur, Hernan
+
+
+## Documentation
+
+El presente contiene la 1er pre entrega del proyecto final del curso de JavaScrip de CoderHouse
+
+## Feedback
+
+Este proyecto es solamente de uso para la primera pre entrega del proyecto final del curso de JavaScrips puede o no usarse en el proyecto final.
+## Used By
+
+Este proyecto se utiliza para la interacion con el usuario, que de acuerdo a su edad, puede llegar a obtener una bonificacion en la inscripcion al curso, que en base a una funcion se le calcula el iva al valor total del curso, dando como resultado el valor final a abonar, por otro lado,
+mediante un ciclo while, se le permite ingresar su nombre y automaticamente confirmarle y asignarle un numero de inscripcion dandole la bienvenida al curso.
+
+Se aconseja ver toda la funcionabilidad desde el HTML denominado "CONTACTO", ya que los demas en la consola dan unos errores de conexion.
---
+++ scripts/main.js
@@ -0,0 +1,50 @@
+let porcentaje = 30;
+let edad = parseInt(prompt("¿Cual es tu edad?"));
+alert("Tu edad ingresada es " + edad);
+console.log("Tu edad es ", edad);
+let precio = 100000;
+let descuento = 0;
+let total = precio;
+if (edad <= 18) {
+  descuento = precio * (porcentaje / 100);
+  alert("¡Felicitaciones de acuerdo a tu edad ingresada tienes una bonificacion del " + porcentaje + "%!");
+  console.log("¡Felicitaciones de acuerdo a tu edad ingresada tienes una bonificacion del " + porcentaje + "%!");
+} else {
+  edad > 19;
+}
+total = precio - descuento;
+alert("De acuerdo a tu edad el curso tiene un valor de $ " + total);
+console.log("De acuerdo a tu edad el curso tiene un valor de $ " + total);
+
+const suma = (a,b) => a + b
+const iva = x => x * 0.21
+let valorCurso = total
+let precioFinal = suma(valorCurso, iva(valorCurso))
+alert("EL valor del curso mas iva es de $" + precioFinal)
+console.log("El valor del curso mas iva es de $" + precioFinal);
+
+
+let ingresante = 0;
+while (ingresante < 10) {
+  nombreIngresante = prompt("¿Cual es tu nombre completo?");
+  if (nombreIngresante == "") {
+    alert("El nombre tiene que tener algun caracter");
+    console.log("El nombre tiene que tener algun caracter");
+    continue;
+  }
+  if (nombreIngresante.length < 8) {
+    alert("El nombre debe tener al menos 8 caracteres");
+    console.log("El nombre debe tener al menos 8 caracteres");
+    continue;
+  }
+  if (nombreIngresante.length > 30) {
+    alert("El nombre no puede exceder los 30 caracteres");
+    console.log("El nombre no puede exceder los 30 caracteres");
+    continue;
+  }
+  ingresante++;
+  alert("Felicitaciones ya estas inscripto, tu numero de ingreso es " + ingresante + " Bienvenido a nuestro curso " + nombreIngresante);
+  console.log("Felicitaciones ya estas inscripto, tu numero de ingreso es " + ingresante + " Bienvenido a nuestro curso " + nombreIngresante)
+}
+
+
