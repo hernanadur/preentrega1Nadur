34f518e2ec1de82fbb70f5dc694d0a8c50e6dcbf
Hernan Nadur <hernanadur@gmail.com>
2023-04-19 10:59:29 -0300

    2da pre entrega JS

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 # Project Title
 
-Primer Pre entrega de Proyecto final GESM
+Segunda Pre entrega de Proyecto final GESM
 ## Authors
 
 - Nadur, Hernan
@@ -9,14 +9,21 @@ Primer Pre entrega de Proyecto final GESM
 
 ## Documentation
 
-El presente contiene la 1er pre entrega del proyecto final del curso de JavaScrip de CoderHouse
+El presente contiene la 2da pre entrega del proyecto final del curso de JavaScrip de CoderHouse
 
 ## Feedback
 
-Este proyecto es solamente de uso para la primera pre entrega del proyecto final del curso de JavaScrips puede o no usarse en el proyecto final.
+Este proyecto es solamente de uso para la segunda pre entrega del proyecto final del curso de JavaScrips puede o no usarse en el proyecto final.
 ## Used By
 
-Este proyecto se utiliza para la interaccion con el usuario, que de acuerdo a su edad, puede llegar a obtener una bonificacion en la inscripcion al curso, que en base a una funcion se le calcula el iva al valor total del curso, dando como resultado el valor final a abonar, por otro lado,
+Este proyecto se utiliza para la interaccion con el usuario inicialmente para la seleccion de curso que desea realizar, establecidos por una class, presentado mediante un array numero de ID que representa la oferta de cursos mediante un alert (en consola se presentan todos los datos), mediante una funcion se le solicita al usuario indicar una opcion correcta par poder continuar el proceso, una vez elegido se informa en consola.
+
+Posteriormente se le solicita la edad al usuario mediante un alert,que de acuerdo a su edad, puede llegar a obtener una bonificacion en la inscripcion al curso, que en base a una funcion se le calcula el iva al valor total del curso, dando como resultado el valor final a abonar, por otro lado,
 mediante un ciclo while, se le permite ingresar su nombre y automaticamente confirmarle y asignarle un numero de inscripcion dandole la bienvenida al curso.
 
+Por ultimo se le solicita al usuario el modo de pago mediante una funcion, en caso de ser en cuotas se le adiciona un interes al valor del curso (el cual ya tiene una bonificacion, en caso de corresponder, y ademas tiene el iva agregado), se le brinda una opcion de cuotas disponibles donde cada una tiene una tasa de interes distinta, una vez seleccionado se le informa al usuario el importe final en un alert y se muestra en la consola.
+
 Se aconseja ver toda la funcionabilidad desde el HTML denominado "CONTACTO", ya que los demas en la consola dan unos errores de conexion.
+
+
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,47 @@
+class Curso {
+  constructor(id, nombre, precio) {
+    this.id = id;
+    this.nombre = nombre.toUpperCase();
+    this.precio = parseFloat(precio);
+  }
+}
+let misCursos = [
+  new Curso(1, "Primer Nivel de Supervivencia", 100000),
+  new Curso(2, "Segundo Nivel de Supervivencia", 120000),
+  new Curso(3, "Tercer Nivel de Supervivencia", 140000),
+  new Curso(4, "Tactico de Supervivencia", 160000),
+  new Curso(5, "Primitivo de Supervivencia", 180000),
+];
+
+console.log(
+  " Todos mis cursos son: ",
+  misCursos.map((unCurso) => unCurso.nombre)
+);
+
+let identificadores = misCursos.map((unCurso) => unCurso.id);
+
+const CURSOS_PERMITIDOS = [1, 2, 3, 4, 5];
+const MENSAJE_CURSOS_PERMITIDOS =
+  "¿A que curso deseas inscribirte? --> " + identificadores.join(", ");
+
+function pedirCursos() {
+  let cursosPermitidos = parseInt(prompt(MENSAJE_CURSOS_PERMITIDOS));
+  while (!CURSOS_PERMITIDOS.includes(cursosPermitidos)) {
+    cursosPermitidos = parseInt(prompt(MENSAJE_CURSOS_PERMITIDOS));
+  }
+  return cursosPermitidos;
+}
+
+let cursosPermitidos = pedirCursos();
+
+console.log("el curso elegido es el ", cursosPermitidos);
+
 let edad = parseInt(prompt("¿Cual es tu edad?"));
 alert("Tu edad ingresada es " + edad);
 console.log("Tu edad es ", edad);
-let precio = 100000;
+
+let encontrado = misCursos.find((curso) => curso.id === cursosPermitidos);
+let precio = encontrado.precio;
 let descuento = 0;
 let porcentaje = 30;
 let total = precio;
@@ -28,12 +68,12 @@ const suma = (a, b) => a + b;
 const iva = (x) => x * 0.21;
 let valorCurso = total;
 let precioFinal = suma(valorCurso, iva(valorCurso));
-alert("El valor del curso mas iva es de $" + precioFinal);
-console.log("El valor del curso mas iva es de $" + precioFinal);
+alert("El valor del curso MAS iva es de $" + precioFinal);
+console.log("El valor del curso MAS iva es de $" + precioFinal);
 
 let ingresante = 0;
-while (ingresante < 5) {
-  nombreIngresante = prompt("¿Cual es tu nombre completo?");
+while (ingresante < 2) {
+  nombreIngresante = prompt("¿Cual es tu nombre completo?").toUpperCase();
   if (nombreIngresante == "") {
     alert("El nombre tiene que tener algun caracter");
     console.log("El nombre tiene que tener algun caracter");
@@ -63,3 +103,72 @@ while (ingresante < 5) {
       nombreIngresante
   );
 }
+
+const MODO_DE_PAGO = parseInt(
+  prompt("¿Lo quiere abonar al contado o en cuotas? 1:CONTADO y 2:CUOTAS")
+);
+
+function calcularIncremento(cantidadCuotas, precioFinal) {
+  let interes = 0;
+  switch (cantidadCuotas) {
+    case 3:
+      interes = precioFinal * 0.2;
+      break;
+    case 6:
+      interes = precioFinal * 0.45;
+      break;
+    case 9:
+      interes = precioFinal * 0.65;
+      break;
+    case 12:
+      interes = precioFinal * 0.9;
+      break;
+    default:
+      break;
+  }
+  return interes;
+}
+
+const CUOTAS_PERMITIDAS = [3, 6, 9, 12];
+const MENSAJE_CANTIDAD_CUOTAS =
+  "¿En cuantas cuotas quieres hacer tu compra? --> " +
+  CUOTAS_PERMITIDAS.join(", ");
+
+function pedirCantidadDeCuotas() {
+  let cantidadCuotas = parseInt(prompt(MENSAJE_CANTIDAD_CUOTAS));
+  while (!CUOTAS_PERMITIDAS.includes(cantidadCuotas)) {
+    cantidadCuotas = parseInt(prompt(MENSAJE_CANTIDAD_CUOTAS));
+  }
+  return cantidadCuotas;
+}
+
+let cantidadCuotas = 1;
+let interes = 0;
+let costoTotal = precioFinal;
+let costoPorCuota = costoTotal / cantidadCuotas;
+
+if (MODO_DE_PAGO == 2) {
+  cantidadCuotas = pedirCantidadDeCuotas();
+  interes = calcularIncremento(cantidadCuotas, precioFinal);
+  costoTotal = costoTotal + interes;
+  costoPorCuota = costoTotal / cantidadCuotas;
+}
+
+let mensaje =
+  "El ingresante debera abonar un total de $" +
+  costoTotal.toFixed(2) +
+  ", en un total de " +
+  cantidadCuotas +
+  " cuotas, con un valor de $" +
+  costoPorCuota.toFixed(2) +
+  " por cada cuota";
+
+if (cantidadCuotas == 1) {
+  mensaje =
+    "El ingresante debera abonar un total de $" +
+    costoTotal.toFixed(2) +
+    ", en un unico pago";
+}
+
+alert(mensaje);
+console.log(mensaje);
